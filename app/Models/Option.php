9702c630b28f19904344e6014eaf4b21a431e585
To'xtasin Jo'rayev <olmalar1999@gmail.com>
2022-12-14 01:01:08 +0500

    14.12.2022 tested

--- app/Models/Option.php
+++ app/Models/Option.php
@@ -5,14 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Product extends Model
+class Option extends Model
 {
     use HasFactory;
 
-    protected $fillable = [
-        'name',
-        'slug',
-        'description',
-        'price'
-    ];
+    protected $table = "chpn_options";
 }
